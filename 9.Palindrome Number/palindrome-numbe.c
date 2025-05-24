09560ff343670a5399c5fbaee3561ea225329ff9
ckhater <ckhater@dell.com>
2025-05-24 02:50:08 -0400

    palindrome

---
+++ 9.Palindrome Number/palindrome-numbe.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPalindrome(int x) {
+    int l;
+
+    if(x >= 0 && x < 10)
+    {
+        return true;
+    }
+    l = (int)log10(fabs(x)) + 1;
+    if(x < 0)
+        l++;
+    char s[l + 1];
+    sprintf(s, "%d", x);
+     int i = 0;
+     int j = l;
+
+     while(i < l/2)
+     {
+        if(s[i] != s[j - 1])
+        {
+            return(false);
+        }
+        i++;
+        j--;
+     }
+     return(true);
+}
+
+int main()
+{
+    int x = 121;
+    printf("%d", isPalindrome(x));
+    return 0;
+}
